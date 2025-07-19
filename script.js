58d4f2d6c23981355d357c567add157c1a8f2098
ElenaWikoff <elenawikoff@gmail.com>
2025-07-18 20:03:52 -0500

    Add 16x16

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM Content Loaded");
 
+    const container = document.getElementById("container");
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Hello World!");
+    for (i = 0; i < 16; i++) {
+        for (j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            square.setAttribute("id", `square-${i}-${j}`);
+            square.classList.add("square");
+
+            // Add hover mouse events
+            square.addEventListener('mouseenter', handleSquareEnter);
+            square.addEventListener('mouseleave', handleSquareLeave);
+
+            // Add squares container grid
+            container.appendChild(square);
+        }
+    }
 });
+
+const handleSquareEnter = (event) => {
+    const square = event.target;
+    square.classList.add("over");
+}
+
+const handleSquareLeave = (event) => {
+    const square = event.target;
+    square.classList.remove("over");
+} 
