54671046393ba159319aad33b1eadcd95616bbc2
ElenaWikoff <elenawikoff@gmail.com>
2025-07-23 18:44:52 -0500

    Add shake button and incremental coloring

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-var mouseDown = false;
 
+// Keep track of whether mouse is down
+var mouseDown = false;
 window.addEventListener("mousedown", () => { mouseDown = true });
 window.addEventListener("mouseup", () => { mouseDown = false });
 
+// Start javascript on DOM load
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM Content Loaded");
 
+    // Initialize squares
     createSquares(16);
 });
 
+// Clear etch-a-sketch of squares (in order to repopulate)
 const clearSquares = () => {
     const container = document.getElementById("container");
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => container.removeChild(square));
 }
 
+// Create grid of rxr squares
 const createSquares = (r) => {
     const container = document.getElementById("container");
 
@@ -23,6 +28,7 @@ const createSquares = (r) => {
         for (j = 0; j < r; j++) {
             const square = document.createElement("div");
             square.setAttribute("id", `square-${i}-${j}`);
+            square.setAttribute("data-value", 0);
             square.classList.add("square");
 
             // Set width and height of squares
@@ -39,6 +45,7 @@ const createSquares = (r) => {
     }
 }
 
+// Prompt user for number of squares per row/col they want on the etch-a-sketch
 const setSquares = () => {
     let r = prompt("Please input how many rows and columns for the grid (max: 100):");
     if (r > 100) {
@@ -48,16 +55,24 @@ const setSquares = () => {
     createSquares(r);
 }
 
+// On square enter while mouse down, darken the square by 10%.
 const handleSquareEnter = (event) => {
     event.preventDefault();
     if (mouseDown) {
         const square = event.target;
-        square.classList.add("over");
+        let value = Number(square.getAttribute("data-value"));
+        if (!value) {
+            square.setAttribute("data-value", 1);
+        } else if (value < 10) {
+            value += 1;
+            square.setAttribute("data-value", value);
+        }
     }
-    console.log(mouseDown);
 }
 
-const handleSquareLeave = (event) => {
-    const square = event.target;
-    square.classList.remove("over");
-} 
+// Clear squares of opacity
+const shake = () => {
+    const container = document.getElementById("container");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => square.setAttribute("data-value", 0));
+}
